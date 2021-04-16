e7882f816dc72e1ca977bef96b05d25c73a93786
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-16 21:12:27 +0000

    added last crude version of task 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,30 @@
+# This manifest handles the task of creating a custom HTTP header response
+# does not setup server like task 0 just the custom header !!!!
+exec {'Install nginx':
+    path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+    command => 'sudo apt-get -y update; sudo apt-get -y install nginx'
+}
+exec {'mkdirectorys':
+    path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+    command => 'mkdir -p /data/web_static/releases/test/; mkdir -p /data/web_static/shared/'
+    require => Exec['Install nginx'],
+}
+file {'create index.html':
+  path    => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>'
+  ensure  => 'present',
+  mode    => '0664',
+  content => 'I love Puppet',
+  require => Exec['mkdirectorys'],
+}
+exec {'rm/ln':
+    path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+    command => 'rm -f /data/web_static/current;
+    ln -s /data/web_static/releases/test/ /data/web_static/current;
+    sudo chown -R ubuntu:ubuntu /data/;'
+}
