c41787ba966ccadb8e52f6e8d45710869d0c5236
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-02 18:47:21 +0000

    added task 6 and task 6 html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""
+Description:
+
+METHODS FOR ROUTES >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+One:
+    In this module we start a flask application called flask_app.
+within this application we have a single root '/' call. When the user
+opens the URL it should return "Hello HBNB!" from def hello_world.
+
+Two:
+    The '/hbnb' directory calls function that prints "HBNB".
+Does not use strict_slashes=False like Route directory
+
+Three:
+    The '/c/<text>' will display “C ” followed by the value of the text
+variable (replace underscore _ symbols with a space )...
+Does not use strict_slashes=False like Route directory
+
+Four:
+    The '/python/(<text>)' will display “Python”, followed by the value of
+the text variable (replace underscore _ symbols with a space )...
+The default value of text is “is cool”
+
+Five:
+    The '/number/<n>' will display “n is a number” only if n is an integer
+
+Six:
+    The '/number_template/<n>' display a HTML page only if n is an integer
+
+Application:
+    Port = 5000
+    Host = 0.0.0.0 (public access)
+"""
+from flask import Flask, render_template
+flask_app = Flask(__name__)  # starts flask app
+
+
+@flask_app.route('/', strict_slashes=False)  # 1
+def print_greet():
+    """ Returns String "Hello HBNB!" when route is landed upon """
+    return "Hello HBNB!"
+
+
+@flask_app.route('/hbnb')  # 2
+def print_hbnb():
+    """ Returns String "HBNB" """
+    return "HBNB"
+
+
+@flask_app.route('/c/<text>')  # 3
+def print_text(text):
+    """ prints out given text using flask variable rules """
+    return 'C %s' % text.replace("_", " ")
+
+
+@flask_app.route('/python', strict_slashes=False)  # so dir doesnt throw 404
+@flask_app.route('/python/<text>')  # 4
+def print_text_default(text='is cool'):
+    """ prints out given text using flask variable rules """
+    return 'Python %s' % text.replace("_", " ")
+
+
+@flask_app.route('/number/<int:n>')  # 5
+def print_if_num(n):
+    """ prints out num if it is a number """
+    return '%i is a number' % n
+
+
+@flask_app.route('/number_template/<int:n>')  # 6
+def render_html_int(n):
+    """ renders html template only if n is a number """
+    return render_template('5-number.html', num=n)
+
+
+@flask_app.route('/number_odd_or_even/<int:n>')  # 7
+def odd_or_even(n):
+    result = ''
+    if n % 2 == 0:
+        result = 'even'
+    else:
+        result = 'odd'
+    return render_template('6-number_odd_or_even.html', result=result)
+
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port=5000)  # sets host and port when main
